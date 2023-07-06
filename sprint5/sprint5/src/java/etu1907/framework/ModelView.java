47722af7f8cd09c4f9cb27143bc12c95018e8617
DiaryRz <119029048+DiaryRz@users.noreply.github.com>
2023-07-06 14:37:15 +0200

    sprint5

---
+++ sprint5/sprint5/src/java/etu1907/framework/ModelView.java
@@ -0,0 +1,16 @@
+package etu1907.framework;
+
+import java.util.HashMap;
+
+public class ModelView {
+    String View;
+
+    public String getView() {
+        return View;
+    }
+
+    public void setView(String View) {
+        this.View = View;
+    }
+    
+}
